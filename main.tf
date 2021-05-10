8edad7fd46f89410329be73538899f187f5b2ab5
RajanikanthGitHub <rajanikanth86@gmail.com>
2021-05-10 21:03:58 +0100

    updated azure-pipeline ymal file

--- main.tf
+++ main.tf
@@ -13,10 +13,19 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "e5c53a81-f5c9-4864-b65a-d6fc347b4e2d"
-  tenant_id       = "1f4beacd-b7aa-49b2-aaa1-b8525cb257e0"
-  client_id        = "d5bb4456-d234-47a4-8628-7506c6f58862"
-  client_secret =   "mh8a26jmG0T~-p_c7iG~T-~owDUPI7tYWF"
+  #subscription_id = "e5c53a81-f5c9-4864-b65a-d6fc347b4e2d"
+  #tenant_id       = "1f4beacd-b7aa-49b2-aaa1-b8525cb257e0"
+  #client_id        = "d5bb4456-d234-47a4-8628-7506c6f58862"
+  #client_secret =   "mh8a26jmG0T~-p_c7iG~T-~owDUPI7tYWF"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf_storage_rsg"
+    storage_account_name = "tfstoragernagelli"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
 }
 
 # creating Resouce group
